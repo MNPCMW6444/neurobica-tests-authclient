63f2fa955f1824b600b9239ed07bbeb05b236107
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-07 23:04:59 +0300

    Update domain.js

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,4 +1,1 @@
-export default process.env.NODE_ENV === "development"
-  ? "http://localhost:5011"
-  : process.env.NODE_ENV === "production" &&
-    "https://snippet-manager-server.herokuapp.com";
+export default "https://neurobica-authtest.herokuapp.com";
