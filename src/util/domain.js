7805ee58774115fdd3af52bac0ab7a532d72d4e0
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-08 15:51:35 +0300

    Update domain.js

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,1 +1,3 @@
-export default "https://neurobica-authtest.herokuapp.com";
+export default process.env.NODE_ENV === "production?"
+  ? "https://neurobica-authtest.herokuapp.com"
+  : "http://localhost:9876";
