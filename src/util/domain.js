30f614b8a98fe3e48f6705b3f78fdf2f7ae897b6
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-08 16:12:00 +0300

    ae

--- src/util/domain.js
+++ src/util/domain.js
@@ -1,3 +1,3 @@
-export default process.env.NODE_ENV === "production?"
+export default process.env.NODE_ENV === "production"
   ? "https://neurobica-authtest.herokuapp.com"
   : "http://localhost:9876";
